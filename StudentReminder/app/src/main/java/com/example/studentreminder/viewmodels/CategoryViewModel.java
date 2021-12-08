6cf42156bfe5c40d68b482292e06eb7cd06733c8
stormray73 <geoffreybear1@gmail.com>
2021-12-08 09:50:52 -0700

    Models and ViewModels created before database implementation

--- StudentReminder/app/src/main/java/com/example/studentreminder/viewmodels/CategoryViewModel.java
+++ StudentReminder/app/src/main/java/com/example/studentreminder/viewmodels/CategoryViewModel.java
@@ -1,4 +1,22 @@
 package com.example.studentreminder.viewmodels;
 
-public class CategoryViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+
+import com.example.studentreminder.models.CategoryItem;
+
+public class CategoryViewModel extends AndroidViewModel {
+    public CategoryViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void saveCategory(String title, String colour){
+        CategoryItem category = new CategoryItem();
+        category.category = title;
+        category.colour = colour;
+
+    }
 }
--- StudentReminder/app/src/main/java/com/example/studentreminder/viewmodels/ToDoViewModel.java
+++ StudentReminder/app/src/main/java/com/example/studentreminder/viewmodels/ToDoViewModel.java
@@ -5,15 +5,28 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.example.studentreminder.models.ToDoItem;
+
 public class ToDoViewModel extends AndroidViewModel {
     public ToDoViewModel(@NonNull Application application) {
         super(application);
     }
 
-    public void saveToDoItem(String title, long dueDate, long remindDate, String reoccur){
-
+    public void saveToDoItem(String title, long dueDate, long remindDate, String reoccur, boolean isCanvasItem){
+        ToDoItem item = new ToDoItem();
+        item.title = title;
+        item.dueDate = dueDate;
+        item.remindDate = remindDate;
+        item.reoccur = reoccur;
+        item.isCanvasItem = isCanvasItem;
     }
-    public void saveToDoItem(String title, long dueDate, long remindDate, String reoccur, int categoryId){
-
+    public void saveToDoItem(String title, long dueDate, long remindDate, String reoccur, boolean isCanvasItem, int categoryId){
+        ToDoItem item = new ToDoItem();
+        item.title = title;
+        item.dueDate = dueDate;
+        item.remindDate = remindDate;
+        item.reoccur = reoccur;
+        item.categoryId = categoryId;
+        item.isCanvasItem = isCanvasItem;
     }
 }
