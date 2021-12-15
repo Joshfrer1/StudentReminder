1ccd850c3b1cc333332d1408729649cb52dbefca
Joshfrer1 <joshfrer@gmail.com>
2021-12-15 11:05:39 -0700

    more changes added, error with getContext() for canvas api

--- StudentReminder/app/src/main/java/com/example/studentreminder/UserProfileFragment.java
+++ StudentReminder/app/src/main/java/com/example/studentreminder/UserProfileFragment.java
@@ -1,4 +1,37 @@
 package com.example.studentreminder;
 
-public class UserProfileFragment {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.studentreminder.api.USUCanvasAPI;
+import com.example.studentreminder.models.UpcomingEvent;
+import com.example.studentreminder.models.User;
+
+import java.util.ArrayList;
+
+public class UserProfileFragment extends Fragment {
+    public UserProfileFragment() {
+        super(R.layout.user_profile);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        USUCanvasAPI api = USUCanvasAPI.getInstance(getContext());
+
+        api.getUser(new USUCanvasAPI.OnRequestCompleteListener<User>() {
+            @Override
+            public void onComplete(User[] result, String errorMessage) {
+                TextView userName = view.findViewById(R.id.userName);
+                userName.setText(result[0].name);
+            }
+        });
+    }
 }
