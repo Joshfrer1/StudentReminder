a6b7c90ac4560a8981c9427e213c7ae740bc553d
Joshfrer1 <joshfrer@gmail.com>
2021-12-17 13:32:34 -0700

    added date picker

--- StudentReminder/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/studentreminder/databinding/FragmentEditEventBinding.java
+++ StudentReminder/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/studentreminder/databinding/FragmentEditEventBinding.java
@@ -4,6 +4,7 @@ package com.example.studentreminder.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +12,6 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
 import androidx.viewbinding.ViewBindings;
 import com.example.studentreminder.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.lang.NullPointerException;
 import java.lang.Override;
 import java.lang.String;
@@ -21,15 +21,19 @@ public final class FragmentEditEventBinding implements ViewBinding {
   private final ConstraintLayout rootView;
 
   @NonNull
-  public final EditText editTextTextPersonName;
+  public final Button date;
 
   @NonNull
-  public final FloatingActionButton save;
+  public final EditText getEvent;
 
-  private FragmentEditEventBinding(@NonNull ConstraintLayout rootView,
-      @NonNull EditText editTextTextPersonName, @NonNull FloatingActionButton save) {
+  @NonNull
+  public final Button save;
+
+  private FragmentEditEventBinding(@NonNull ConstraintLayout rootView, @NonNull Button date,
+      @NonNull EditText getEvent, @NonNull Button save) {
     this.rootView = rootView;
-    this.editTextTextPersonName = editTextTextPersonName;
+    this.date = date;
+    this.getEvent = getEvent;
     this.save = save;
   }
 
@@ -60,20 +64,25 @@ public final class FragmentEditEventBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
-      id = R.id.editTextTextPersonName;
-      EditText editTextTextPersonName = ViewBindings.findChildViewById(rootView, id);
-      if (editTextTextPersonName == null) {
+      id = R.id.date;
+      Button date = ViewBindings.findChildViewById(rootView, id);
+      if (date == null) {
+        break missingId;
+      }
+
+      id = R.id.getEvent;
+      EditText getEvent = ViewBindings.findChildViewById(rootView, id);
+      if (getEvent == null) {
         break missingId;
       }
 
       id = R.id.save;
-      FloatingActionButton save = ViewBindings.findChildViewById(rootView, id);
+      Button save = ViewBindings.findChildViewById(rootView, id);
       if (save == null) {
         break missingId;
       }
 
-      return new FragmentEditEventBinding((ConstraintLayout) rootView, editTextTextPersonName,
-          save);
+      return new FragmentEditEventBinding((ConstraintLayout) rootView, date, getEvent, save);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
--- StudentReminder/app/src/main/java/com/example/studentreminder/EditEventFragment.java
+++ StudentReminder/app/src/main/java/com/example/studentreminder/EditEventFragment.java
@@ -2,63 +2,40 @@ package com.example.studentreminder;
 
 import android.os.Bundle;
 
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditEventFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.studentreminder.databinding.FragmentEditEventBinding;
+import com.example.studentreminder.viewmodels.ToDoViewModel;
+import com.google.android.material.datepicker.MaterialDatePicker;
+
+
 public class EditEventFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EditEventFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditEventFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditEventFragment newInstance(String param1, String param2) {
-        EditEventFragment fragment = new EditEventFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ToDoViewModel viewModel = new ViewModelProvider(requireActivity()).get(ToDoViewModel.class);
+        FragmentEditEventBinding binding = FragmentEditEventBinding.inflate(inflater, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_event, container, false);
+        binding.save.setOnClickListener(view -> {
+
+        });
+        binding.date.setOnClickListener(view -> {
+            MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Select Date")
+                    .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                    .build();
+            datePicker.show(requireActivity().getSupportFragmentManager(), "tag");
+        });
+
+        return binding.getRoot();
     }
 }
